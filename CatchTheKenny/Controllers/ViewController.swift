15ff7e895efdd96b21dc13fe9c5d53cb01cb127d
Chris Hand <cj.hand@gmail.com>
2022-08-14 22:06:08 -0700

    Try out the models, see if they work

--- CatchTheKenny/Controllers/ViewController.swift
+++ CatchTheKenny/Controllers/ViewController.swift
@@ -11,7 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let modelData = ModelData()
+        modelData.loadLevels()
+        modelData.loadScores()
+        modelData.saveScores()
     }
 
 
