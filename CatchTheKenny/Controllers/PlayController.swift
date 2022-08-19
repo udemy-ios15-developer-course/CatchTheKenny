8a72eeef9179ff2e7b52255d66d6a895b3a5b291
Chris Hand <cj.hand@gmail.com>
2022-08-18 17:30:10 -0700

    Figuring out how I will detect misses vs hits

--- CatchTheKenny/Controllers/PlayController.swift
+++ CatchTheKenny/Controllers/PlayController.swift
@@ -5,25 +5,28 @@
 //  Created by Chris Hand on 8/14/22.
 //
 
+/*
+    The way to win a level is to hit the kenny the required number of times, before you miss the limited number of times
+    
+    Each level will adjust the size of the kenny: can be very big to very small
+    Each level will adjust the number of times kenny is hit
+    Each level will adjust the number of times kenny is missed
+    Each level will adjust the movement pattern: linear, bouncing, random
+    Each level will create objects that kenny could either move behind, or bounce off of
+    Each level will have a timelimit
+ 
+    Need to add a bouncing sound, and a splat sound
+ */
+
+
 import UIKit
 
 class PlayController: UIViewController {
 
+    @IBOutlet weak var baseView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.navigationController?.isNavigationBarHidden = true
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
